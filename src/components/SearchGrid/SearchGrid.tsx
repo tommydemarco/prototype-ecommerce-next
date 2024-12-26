004ee759ecdb3fa66b16b890cbd22b40d36d76f4
Tommaso De Marco <tommaso.demarco95@gmail.com>
2024-12-26 23:58:26 +0100

    Adds SearchGrid component boilerplate sor search page

---
+++ src/components/SearchGrid/SearchGrid.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./SearchGrid.module.css";
+import { Product } from "@/types";
+import { ProductCard } from "../ProductCard/ProductCard";
+
+interface SearchGridProps {
+  searchResults: Product[];
+  categories: React.ReactNode;
+  sortParams: React.ReactNode;
+}
+
+export const SearchGrid: React.FC<SearchGridProps> = ({
+  searchResults,
+  categories,
+  sortParams,
+}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.categoriesColumn}>{categories}</div>
+      <div className={styles.productsColumn}>
+        {searchResults.length ? (
+          searchResults.map((product) => {
+            return (
+              <ProductCard
+                layoutType="square"
+                key={product.product_id}
+                product={product}
+              />
+            );
+          })
+        ) : (
+          <div>Your search did not yeald any results.</div>
+        )}
+      </div>
+      <div className={styles.sortColumn}>{sortParams}</div>
+    </div>
+  );
+};
