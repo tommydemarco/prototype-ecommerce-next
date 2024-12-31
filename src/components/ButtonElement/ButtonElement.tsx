c3ac7550484c7e18b7f647ea26e22bc1ca0a210b
Tommaso De Marco <tommaso.demarco95@gmail.com>
2024-12-31 19:32:07 +0100

    Rename wrongly named stylesheet

--- src/components/ButtonElement/ButtonElement.tsx
+++ src/components/ButtonElement/ButtonElement.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./ButtonElementProps.module.css";
+import styles from "./ButtonElement.module.css";
 
 interface ButtonElementProps {
   onClick?: () => void;
